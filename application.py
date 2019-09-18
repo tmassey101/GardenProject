7b1829d779d376879b346b63ac82240cabc8cdc7
tmassey101 <tmassey101@hotmail.com>
2019-09-18 22:46:06 +0100

    Added timeseries chart to /charttest using chart.js

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
+from sqlalchemy.engine import reflection
+import numpy as np
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
-
+#from datatime import datatime, date
 
 load_dotenv()
 
@@ -23,13 +25,14 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
+insp = reflection.Inspector.from_engine(engine)
+print(insp.get_table_names())
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
 
-    return "homepage"
+    return render_template('index.html')
 
 @app.route("/insert/<value>", methods=["GET", "POST"])
 def insert(value):
@@ -43,7 +46,7 @@ def insert(value):
 @app.route("/results", methods=["GET", "POST"])
 def results():
 
-    data = db.execute("SELECT * FROM sensorinputs LIMIT 50").fetchall()
+    data = db.execute("SELECT * FROM sensorinputs LIMIT 50").fetchmany(50)
 
     return render_template('results.html', data = data)
 
@@ -53,3 +56,25 @@ def raw():
     data = db.execute("SELECT * FROM sensorinputs LIMIT 2").fetchall()
 
     return str(data)
+
+@app.route("/charttest", methods=["GET", "POST"])
+def charttest():
+
+    data = db.execute("SELECT id, value, created FROM sensorinputs LIMIT 50").fetchall()
+
+    values = []
+    id = []
+    labels = []
+
+    for i in data:
+        id.append(int(i[0]))
+        values.append(float(i[1]))
+        labels.append(i[2].strftime("%c"))
+
+    print(type(id), type(values), type(labels))
+    print(id[2])
+    print(values[2])
+    print(labels[2])
+    
+
+    return render_template('charttest.html', data = data, id=id, labels=labels, values=values)
