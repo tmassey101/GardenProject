dbfae9a898fd5e9f53f40033158afffa8e2d03be
tmassey101 <tmassey101@hotmail.com>
2019-10-04 20:04:55 +0100

    added new route insertall for inserting differetnt types

--- Client/PiCode.py
+++ Client/PiCode.py
@@ -3,15 +3,37 @@ import requests
 import random
 import time
 
+import time
+
+from board import SCL, SDA
+import busio
+
+from adafruit_seesaw.seesaw import Seesaw
+
+#Define correct connection for i2c sensor
+i2c_bus = busio.I2C(SCL, SDA)
+
+ss = Seesaw(i2c_bus, addr=0x36)
+
 #setup time loop
 i = 0
 
-while(i < 1000):
+while(True):
+    
+    
+    # read moisture level through capacitive touch pad
+    touch = ss.moisture_read()
+
+    # read temperature from the temperature sensor
+    temp = ss.get_temp()
+    
     
     #create a value to post
-    value = 20 + random.randint(1,2) + random.randint(1,10)/10
-    print("value to insert: "+str(value))
-
+    #value = 21 + random.randint(1,2) + random.randint(1,10)/10
+    #print("value to insert: "+str(value))
+    
+    value = temp
+    
     #send a request
     url = "https://garden-project.herokuapp.com/insert/"
     r = requests.post(url+str(value))
@@ -22,6 +44,6 @@ while(i < 1000):
 
     #wait for a minute before looping
     i += 1
-    time.sleep(60)
+    time.sleep(2)
 
 print("Aren't you bored?")
