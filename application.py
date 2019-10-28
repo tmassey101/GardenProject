0886c9369b43b3e60fe006bb978a0647be0d944c
tmassey101 <tmassey101@hotmail.com>
2019-10-28 21:48:20 +0000

    moved /test to /

--- application.py
+++ application.py
@@ -45,76 +45,84 @@ def index():
     title = "Home"
     chartType = "'line'"
 
-    #SQL Queries
-    data = db.execute("select date_trunc('hour', created - interval '1 minutes') as interv_start, date_trunc('hour', created - interval '1 minutes')  + interval '1 hours' as interv_end, avg(value) as avgvalue, measuretype from sensorinputs where created >= NOW() - INTERVAL '240 hours' group by measuretype, date_trunc('hour', created - interval '1 minutes') order by interv_start").fetchall()
-    data2 = db.execute("select avg(value) as avgvalue, measuretype from sensorinputs WHERE created >= NOW() - INTERVAL '60 minutes' GROUP BY measuretype ORDER BY measuretype;").fetchall()
-    moisturetrend = db.execute("select date_trunc('hour', created - interval '1 minutes') as interv_start, avg(value) as avgvalue, measuretype from sensorinputs where created >= NOW() - INTERVAL '3 days' AND measuretype = 'moisture' group by measuretype, date_trunc('hour', created - interval '1 minutes') order by interv_start;").fetchall()
-    lastwatering = db.execute("select created from sensorinputs where measuretype = 'watering' order by created desc limit 1")
+    #SQL Query
+    mlQuery = db.execute("select date_trunc('hour', created - interval '1 minutes') as CreatedHour, avg(value) as Value, TRIM(measuretype) as Type from sensorinputs group by MeasureType, CreatedHour order by CreatedHour").fetchall()
 
-    latestwater = []
-    for row in lastwatering:
-        t = row[0]
-        t = t.strftime('%H:%M %d/%m/%Y ')
-        latestwater.append(t)
-        
-    latestwater = latestwater[0]
+    # Create pivoted dataframe for each variable
+    df = pd.DataFrame(mlQuery, columns=['CreatedHr', 'AvgValue', 'Type'])
+    mldf = df.pivot(index='CreatedHr', columns='Type', values='AvgValue')
+    mldf['CreatedHr'] = mldf.index
+    mldf['watering'] = mldf['watering'].fillna(0)
 
-    values = []
-    id = []
-    labels = []
-    measuretype = []  
-    moistureY = []
-    tempY = []
-    datetimeX = []
-    watering = []
+    # Categorical True/False column for watering
+    wateredTimes = mldf.loc[mldf['watering'] == 1]
+    wateredTimes = wateredTimes.index
 
-    for i in data:
+    mldf['mostRecentWater'] = wateredTimes.searchsorted(value = mldf.index) - 1
+    mldf[mldf['mostRecentWater']< 0 ] = 0
+    mldf['mostRecentWater'] = wateredTimes.values[mldf['mostRecentWater']]
+    mldf['wateringElapsed']= (mldf.index - mldf['mostRecentWater']).astype('timedelta64[h]')
 
-        values.append(float(i[2]))
-        labels.append(i[1].strftime("%c"))
-        measuretype.append(str(i[3]))
+    # Calculate rolling average moisture level
+    periods = 4
+    mldf['Prev4Hrs'] =  mldf['moisture'].rolling(min_periods=1, window=periods).mean()
 
-        if ('temp' in str(i[3])):
-            tempY.append(float(i[2]))
+    mldf = mldf.dropna()
 
-        elif ('moisture' in str(i[3])):
-            moistureY.append(float(i[2]))
+    # Compute Correlation for Moisture Level
 
-        elif ('watering' in str(i[3])):
-            watering.append(1)
+    x_train = mldf['moisture']
+    y_train = mldf[['temp','watering','wateringElapsed','Prev4Hrs']]
 
-        else: 
-            print("Error : unknown measure")
+    model = LinearRegression().fit(y_train,x_train)
+    print("Model Coefs = ", model.coef_)
+    print("Model Interc = ", model.intercept_)
 
-    metrics = []
-    key = []
+    y_pred = model.predict(y_train)
+    mldf['moisturePred'] = y_pred
+
+    # Remove first line (zeros due to historical calc)
+    mldf = mldf.iloc[1:]
+
+    ### Create Historical Variables for Plotting
+    print(mldf.columns)
+    plotTemp = floatList(mldf['temp'])
+    plotMoisture = floatList(mldf['moisture']) 
+    plotWatering = floatList(mldf['watering'])
+    plotMoisturePred = floatList(mldf['moisturePred'])
     
-    for i in data2:
-        metrics.append(float(i[0]))
-        key.append(str(i[1]))
+    labels = mldf['CreatedHr'].to_list()
+    labels = [str(i) for i in labels]
 
-    #set desired average moisture level to re-water
-    watering_point = 700
+    # Latest Metrics for dashboard
+    lastTemp = plotTemp[-1]
+    lastMoist = plotMoisture[-1]
+    waterings = mldf.index[mldf['watering'] == 1]
+    latestWater = waterings.to_list()[-1]
     
-    y = np.asarray(moistureY)
-    x = np.asarray(labels).reshape((-1,1))
-    x2 = np.arange(len(moistureY)).reshape((-1,1))
 
-    model = LinearRegression().fit(x2, y)
-    moistureCoef = float(model.coef_)
-    moistureIntercept = model.intercept_
+    ### Set desired average moisture level to re-water
+    watering_point = 500
+
+    ### Calculate linear moisture trend and rewater point
+    TrendDays = 3
+    Moisture3d = plotMoisture
+
+    y = np.asarray(Moisture3d)
+    x = np.arange(len(Moisture3d)).reshape((-1,1))
+
+    linearModel = LinearRegression().fit(x, y)
+    linMoistureCoef = float(linearModel.coef_)
+    linMoistureIntercept = linearModel.intercept_
    
-    hr_pred = (watering_point - model.intercept_) / model.coef_
-    
+    hr_pred = (watering_point - linMoistureIntercept) / linMoistureIntercept
     day_pred = hr_pred / 24
     
-
     moistureTrend = []
+    for i in np.arange(0,len(Moisture3d)):
+        moistureTrend.append( (i * linMoistureCoef) + linMoistureIntercept)
 
-    for i in np.arange(0,len(labels)):
-        moistureTrend.append( (i * moistureCoef) + moistureIntercept)
-        
-    return render_template('index.html', labels=labels, values=values, chartType=chartType, measuretype=measuretype, title=title, metrics=metrics, day_pred=day_pred, moistureTrend=moistureTrend, latestwater=latestwater)
+    return render_template('test.html', latestWater=latestWater, lastTemp=lastTemp, lastMoist=lastMoist, plotTemp=plotTemp, plotMoisture=plotMoisture, plotWatering=plotWatering, plotMoisturePred=plotMoisturePred, labels=labels, chartType=chartType, title=title, day_pred=day_pred, moistureTrend=moistureTrend)
 
 
 @app.route("/insert/<value>", methods=["GET", "POST"])
@@ -285,12 +293,12 @@ def test():
     labels = mldf['CreatedHr'].to_list()
     labels = [str(i) for i in labels]
 
+    # Latest Metrics for dashboard
     lastTemp = plotTemp[-1]
     lastMoist = plotMoisture[-1]
     waterings = mldf.index[mldf['watering'] == 1]
     latestWater = waterings.to_list()[-1]
-
-    print(latestWater)
+    
 
     ### Set desired average moisture level to re-water
     watering_point = 500
@@ -302,15 +310,49 @@ def test():
     y = np.asarray(Moisture3d)
     x = np.arange(len(Moisture3d)).reshape((-1,1))
 
-    model = LinearRegression().fit(x, y)
-    moistureCoef = float(model.coef_)
-    moistureIntercept = model.intercept_
+    linearModel = LinearRegression().fit(x, y)
+    linMoistureCoef = float(linearModel.coef_)
+    linMoistureIntercept = linearModel.intercept_
    
-    hr_pred = (watering_point - model.intercept_) / model.coef_
+    hr_pred = (watering_point - linMoistureIntercept) / linMoistureIntercept
     day_pred = hr_pred / 24
     
     moistureTrend = []
     for i in np.arange(0,len(Moisture3d)):
-        moistureTrend.append( (i * moistureCoef) + moistureIntercept)
-            
+        moistureTrend.append( (i * linMoistureCoef) + linMoistureIntercept)
+
+    
+    ####### NEW PREDICTION SECTION #############
+    rollingPeriods = -4
+    lastElapsed = floatList(mldf['wateringElapsed'])
+
+    histMoisture = np.mean(plotMoisture[rollingPeriods:]) #base historical moisture on last x periods
+    futTemp = plotTemp[-24:] #base historical temps on last 24 hours
+    
+
+    hours = 72 #enter days of history as future temps
+    for i in range(hours-24): #append multple days as set
+        futTemp.append(futTemp)
+
+    histWater = lastElapsed[-1]
+    futElapsed = [histWater]
+
+    i = 1
+    while i < 72:
+        futElapsed.append(futElapsed[0]+i)
+        i+=1
+
+    print(len(futTemp))
+    print(len(futElapsed))
+    
+    
+    futureDF = pd.DataFrame()
+    futureDF['futTemp'] = futTemp
+    futureDF['futElapsed'] = futElapsed                
+    
+
+    print(futureDF.columns)
+    print(futureDF.head(5))
+    
+
     return render_template('test.html', latestWater=latestWater, lastTemp=lastTemp, lastMoist=lastMoist, plotTemp=plotTemp, plotMoisture=plotMoisture, plotWatering=plotWatering, plotMoisturePred=plotMoisturePred, labels=labels, chartType=chartType, title=title, day_pred=day_pred, moistureTrend=moistureTrend)
